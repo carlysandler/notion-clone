811853fad20400df9254110d88241853ba299314
Carly <carly.sandler@utilizecore.com>
2024-01-05 10:02:41 -0500

    auth with clerk

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,13 @@
 "use client"
-
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+import Link from "next/link";
 import { Button } from "@/components/ui/button"
 import { ArrowRight } from "lucide-react"
+import { Spinner } from "@/components/spinner";
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -16,10 +20,19 @@ export const Heading = () => {
         Notion is the connected workspace where <br />
         better, faster work happens.
       </h3>
-      <Button >
-        Let's Begin
-        <ArrowRight className="h-4 w-4 ml-2"/>
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Jotion
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
     </div>
   )
 }
